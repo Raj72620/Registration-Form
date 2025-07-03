2b61bbdf43f23bd7612f2793adb89538cce8b873
Raj Nishanth <nishanthraj9618@gmail.com>
2025-07-03 23:17:54 +0530

    recent updates with navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import { FaUserGraduate } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="container">
-        <h1 className="navbar-brand">College Registration</h1>
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <FaUserGraduate className="navbar-icon" />
+          <h1>College Registration Portal</h1>
+        </div>
+        <div className="navbar-links">
+          <a href="/" className="nav-link active">Home</a>
+          <a href="/users" className="nav-link">Registrations</a>
+        </div>
       </div>
     </nav>
   );
